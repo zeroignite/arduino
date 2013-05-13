803290df7a2db0165bb96883a063381077175198
zeroignite <zeroignite@kirjava.(none)>
2013-05-12 23:40:04 -0600

    got fundamental brightness code up

--- testing/dimmertest/dimmertest.ino
+++ testing/dimmertest/dimmertest.ino
@@ -1,30 +1,34 @@
 int pwm = 9; //pin 9 is PWM
+int control= 5; //pin 5 is optoisoloator switch
 
 
 void setup() {
     TCCR1B = TCCR1B & 0b11111000 | 0x01 ; //set fuse for 31KHz frequency
+    
     pinMode(pwm, OUTPUT);
+    pinMode(control, OUTPUT);
+    
+    analogWrite(pwm, 128); //turn on the HV pwm
+    
+    
 }
 
-void HVon(){ //this function turns the HV line on
-    digitalWrite(pwm, 128);
-}
-
-void HVoff(){ //this functions turns the HV line off
-    digitalWrite(pwm, 0);
-}
 
 void loop() {
+  float brightness = 25; //brightness in percent
+  //meaningful brightness is 12.5 to 100 in 2x steps
+  float cycle = 5; //cycle time in milliseconds 
   
-int strobetime = 5000; //time of full cycle (microseconds)
-int brightness = 9; //brightness steps, 10 is max
-
-HVon(); //power on
-delayMicroseconds(brightness * 500); //500 is microseconds per brightness step
-
-HVoff(); //power off
-delayMicroseconds( (brightness - 10) * 500);
-
+  float ontime = brightness / 100 * cycle;
+  float offtime = cycle - ontime;
+  
+  if(brightness > 10) {
+    digitalWrite(control, 1);
+    delay(ontime);
+    }
+  
+  digitalWrite(control, 0);
+  delay(offtime);
   }
 
 
