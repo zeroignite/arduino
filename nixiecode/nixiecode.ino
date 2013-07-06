82881a420a29a3d17fe2eb2a4f51d5b8783066aa
zeroignite <zeroignite@kirjava.(none)>
2013-07-06 17:56:40 -0600

    added explicit C0 public domain licensing

--- nixiecode/nixiecode.ino
+++ nixiecode/nixiecode.ino
@@ -1,6 +1,18 @@
 // nixiecode
 // "Zeroignite" Tenzin Beck, 2013
-// released into the public domain
+// tzbeck@gmail.com
+//
+//To the extent possible under law, the author has dedicated all 
+//copyright and related and neighboring rights to this software to the 
+//public domain worldwide. This software is distributed without any warranty.
+//
+//Full legal information can be found at 
+// http://creativecommons.org/publicdomain/zero/1.0/
+
+
+//=============================
+
+#include <EEPROM.h> //needed to write to nonvolatile memory
 
 //=============================
 // handy macros
